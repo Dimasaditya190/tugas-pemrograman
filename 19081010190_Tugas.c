207911b4ab1dea3d3bbb5bec4482340a3db4df66
Dimasaditya190 <dimasap343@gmail.com>
2020-09-23 11:47:02 +0700

    first coomit

---
+++ 19081010190_Tugas.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Nama : Dimas Aditya\n");
+	printf("TTL : Surabaya, 18 Oktober 2000\n");
+	printf("Alamat : Wonosari 127-A\n");
+	printf("Agama : Islam\n");
+	printf("Kewarganegaraan : Indonesia\n");
+	printf("No.Telp : 082229070147");
+	
+	return 0;
+}
